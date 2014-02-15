714fee98bc1524b69c3005519d9e9f1dae2e6be8
Adrien Cahen <onlineallthetime@gmail.com>
2014-02-14 19:32:32 -0800

    it works

--- public/client.js
+++ public/client.js
@@ -19,15 +19,68 @@ jQuery(function(){
 
 
   socket.on('roster', function (roster) { 
-    console.log('roster', roster);
+    var $roster = $('.roster').empty();
+    $.each(roster, function() {
+      $roster.append(
+        $('<li>').text(this.name)
+      );
+    });
   });
 
-  socket.on('slash', function () { 
-    console.log('slash', arguments);
+  var $convo = $('.conversation')
+    , $compose = $('form.compose')
+    , $input = $compose.find('input');
+
+  function appendMessage() {
+    var d = new Date(this.time);
+    $convo.append(
+      $('<li>').append(
+        $('<time>').text(d.toLocaleString()).attr('datetime', d.toISOString()),
+        $('<q>').text(this.text).attr('cite',this.user.name)
+      )
+    );
+  }
+
+  socket.on('history', function (history) { 
+    $convo.empty();
+    if(history.length) {
+      $convo.removeClass('empty');
+      $.each(history, appendMessage);
+    }
+    else {
+      $convo.addClass('empty');
+    }
   });
 
-  socket.on('conversation', function () { 
-    console.log('conversation', arguments);
+  socket.on('conversation', function (conversation) { 
+    appendMessage.call(conversation);
   });
 
+  socket.on('slash', function (msg, cls) { 
+    var $help = $('<div>').html(msg).addClass('flash '+cls);
+    $help.insertAfter($compose);
+    setTimeout(function(){ $help.fadeOut(); }, 5000);
+  });
+
+
+  $compose.submit(function(e){
+    e.preventDefault();
+
+    var val = $input.val()
+      , valParts = val.split(' ');
+
+    switch(valParts[0]) {
+      case "/join":
+        if(valParts[1].match(/^\w/)) {
+          location.assign('/room/'+valParts[1]);
+          break;
+        }
+
+      default:
+        socket.emit('speech', val);
+    }
+    $input.val('');
+  });
+
+  $input.focus();
 });
